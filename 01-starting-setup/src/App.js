caedd7b5a76c9d0ea34bd0798e2a05c36baae6f2
Alexandre Tavares <alexandre.tavares@wundermanthompson.com>
2022-11-19 00:20:09 +0000

    Master Commit

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,53 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "House Rent",
+      amount: 428.32,
+      date: new Date(2021, 2, 22),
+    },
+    { id: "e3", title: "Spotify Sub", amount: 10, date: new Date(2021, 2, 13) },
+    {
+      id: "e4",
+      title: "Netflix Sub",
+      amount: 14.42,
+      date: new Date(2021, 2, 2),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
